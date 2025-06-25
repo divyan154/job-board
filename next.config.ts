f6c277ebee3481bc519222a0317c0c657fba9c22
Divyansh sharma <divyanshsharmakotarajasthan@gmail.com>
2025-06-25 16:13:12 +0530

    fix0

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,18 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Allow production builds even with type errors
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Allow production builds even with lint errors
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname, "src"),
-    };
-    return config;
-  },
-};
 
-export default nextConfig;
+
+
+
+ 
+  
+}
+   
+  export default nextConfig
